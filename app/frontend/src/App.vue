b988156ed3b6bee7de88a21394e0c7cf43c846f0
a.isaeva <isaeva.nastya04@gmail.com>
2026-03-29 13:32:49 +0300

    компонент шапки приложения + стили для него

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+  import Header from './Header.vue';
+  
 </script>
 
 <template>
- 
+    <Header></Header>
+    <router-view />
 </template>
 
 <style scoped>
---
+++ app/frontend/src/Header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="header">
+        <h1>HR-система</h1>
+        <nav class="nav-panel">
+            <div class="nav-item">Организации</div>
+            <div class="nav-item">Отделы</div>
+            <div class="nav-item">Должности</div>
+        </nav>
+    </div>
+</template>
