86a2651ca3cf7341d275415e934f572aa6ec2d6e
a.isaeva <isaeva.nastya04@gmail.com>
2026-03-30 05:40:22 +0300

    CRUD для организаций (и импорты в modules)

--- app/backend/src/organization/organization.service.ts
+++ app/backend/src/organization/organization.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { Organization } from './entities/organization.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrganizationService {
-  create(createOrganizationDto: CreateOrganizationDto) {
-    return 'This action adds a new organization';
+  constructor(
+      @InjectRepository(Organization)
+      private readonly organizationRepository: Repository<Organization>
+    ){}
+  async create(createOrganizationDto: CreateOrganizationDto) {
+    const org = this.organizationRepository.create(createOrganizationDto)
+    return await this.organizationRepository.save(org)
   }
 
-  findAll() {
-    return `This action returns all organization`;
+  async findAll() {
+    return await this.organizationRepository.find({
+      relations: ['departments']
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} organization`;
+  async findOne(id: number) {
+    const org = await this.organizationRepository.findOne({
+      where:{organization_id: id},
+      relations: ['departments']
+     })
+    if (!org) throw new NotFoundException(`Organization ${id} not found`)
+    return org
   }
 
-  update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
-    return `This action updates a #${id} organization`;
+  async update(id: number, updateOrganizationDto: UpdateOrganizationDto) {
+    const org = await this.findOne(id)
+    const updated = Object.assign(org, updateOrganizationDto)
+    return await this.organizationRepository.save(updated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} organization`;
+  async remove(id: number) {
+    const org = await this.findOne(id)
+    return await this.organizationRepository.softRemove(org);
   }
 }
