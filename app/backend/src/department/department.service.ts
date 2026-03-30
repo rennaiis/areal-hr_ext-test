07d7cbf44537034c36c4d1a51372d738f0d4075c
a.isaeva <isaeva.nastya04@gmail.com>
2026-03-30 04:59:16 +0300

    CRUD для отделов (backend)

--- app/backend/src/department/department.service.ts
+++ app/backend/src/department/department.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Department } from './entities/department.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DepartmentService {
-  create(createDepartmentDto: CreateDepartmentDto) {
-    return 'This action adds a new department';
+  constructor(
+    @InjectRepository(Department)
+    private readonly departmentRepository:Repository<Department>
+  ){}
+
+  async create(createDepartmentDto: CreateDepartmentDto) {
+    const department = this.departmentRepository.create(createDepartmentDto)
+    return await this.departmentRepository.save(department)
   }
 
-  findAll() {
-    return `This action returns all department`;
+  async findAll() {
+    return await 
+      this.departmentRepository.find({
+      relations: ['organization', 'parent_department']
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} department`;
+ async findOne(id: number) {
+    const dep = await this.departmentRepository.findOne({
+      where: {department_id: id},
+      relations: ['organization', 'parent_department']
+    })
+    if (!dep) throw new NotFoundException(`department ${id} not found`)
+    return dep
   }
 
-  update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
-    return `This action updates a #${id} department`;
+  async update(id: number, updateDepartmentDto: UpdateDepartmentDto) {
+    const dep = await this.findOne(id)
+    const updated = Object.assign(dep, updateDepartmentDto)
+    return await this.departmentRepository.save(updated)
   }
+  
 
-  remove(id: number) {
-    return `This action removes a #${id} department`;
+
+  async remove(id: number) {
+    const dep = await this.findOne(id);
+    return await this.departmentRepository.softRemove(dep);
+
   }
 }
