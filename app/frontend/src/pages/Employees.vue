90ba4415d4492e1666c91b578d2e81602b09b0b7
a.isaeva <isaeva.nastya04@gmail.com>
2026-04-13 02:27:45 +0300

    css и основная разметка для страницы сотрудников

--- app/frontend/src/pages/Employees.vue
+++ app/frontend/src/pages/Employees.vue
@@ -2,5 +2,31 @@
 </script>
 
 <template>
-    <h3>Сотрудники</h3>
+    <div class="button-row">
+        <button>Нанять сотрудника</button>
+        <button>Организации и отделы</button>
+        <button>Должности</button>
+    </div>
+    
+
+    <div class="table-grid">
+        <div class="table-header">ФИО</div>
+        <div class="table-header">Организация</div>
+         <div class="table-header">Отдел</div>
+        <div class="table-header">Должность</div>
+        <div class="table-header">Зарплата</div>
+        <div class="table-header">Действия</div>
+        
+
+        <div class="table-item">Иван Иванов</div>
+        <div class="table-item">ООО "Компания"</div>
+        <div class="table-item">it</div>
+        <div class="table-item">Программист</div>
+        <div class="table-item">50 000 ₽</div>
+        <div class="button-row table-item" >
+            <button>Изменить</button>
+            <button>Уволить</button>
+        </div>
+    </div>
+
 </template>
