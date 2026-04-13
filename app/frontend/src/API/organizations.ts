6ce47ca55bcbed0879fb95f17d1fd14dd6f74bc1
a.isaeva <isaeva.nastya04@gmail.com>
2026-04-13 03:51:57 +0300

    API для организаций

--- app/frontend/src/API/organizations.ts
+++ app/frontend/src/API/organizations.ts
@@ -1,36 +1,38 @@
-const URL = "http://localhost:3000/positions"
+const URL = "http://localhost:3000/organizations"
 
-interface Position {
+
+interface Organization{
     name: string;
     comment?: string;
 }
 
+
 export async function getAll(){
     const res =  await fetch(URL)
-    if (!res.ok) throw new Error("can't get positions")
+    if (!res.ok) throw new Error("can't get organizations")
     return await res.json()
 }
 
 export async function getOne(id: number) {
     const res =  await fetch(`${URL}/${id}`)
-    if (!res.ok) throw new Error(`can't find position ${id}`)
+    if (!res.ok) throw new Error(`can't find organizations ${id}`)
     return res.json()
 }
 
-export async function create(pos: Position){
+export async function create(org: Organization){
     const res = await fetch(URL, {
         method: 'POST', 
         headers: {'Content-Type':'application/json'},
-        body: JSON.stringify(pos)
+        body: JSON.stringify(org)
     })
     return res.json()
 }
 
-export async function update(id: number, pos: Position) {
+export async function update(id: number, org: Organization) {
     const res = await fetch(`${URL}/${id}`, {
         method: 'PATCH', 
         headers: {'Content-Type':'application/json'},
-        body: JSON.stringify(pos)
+        body: JSON.stringify(org)
     })
     return res.json()
 }
@@ -39,5 +41,6 @@ export async function remove(id: number) {
     const res=await fetch(`${URL}/${id}`, {
         method: 'DELETE'
     })   
-    if (!res.ok) throw new Error(`can't delete position ${id}`)
+    if (!res.ok) throw new Error(`can't delete organization ${id}`)
+
 }
