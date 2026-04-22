d08d136156d02ef3291adcb07f380ae702510411
a.isaeva <isaeva.nastya04@gmail.com>
2026-04-23 02:15:54 +0300

    увольнение, изменение ЗП и отдела сотрудника

--- app/frontend/src/pages/Employees.vue
+++ app/frontend/src/pages/Employees.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
     import {onMounted, ref, computed} from 'vue'
     import Positions from './Positions.vue';
-    import type { Employee, fullHrOperation} from '../interfaces';
+    import { type Department, type Position, type Employee, type fullHrOperation, type HrOperation, type Organization} from '../interfaces';
     import { getAllEmployees } from '../API/employees';
-    import { getAllOperations } from '../API/hr-operations';
+    import { createOperation, getAllOperations } from '../API/hr-operations';
+    import { getAllPositions } from '../API/positions';
+    import { getAllForOrgFlat } from '../API/departments';
+    import { HrOperationType } from '../../../enums/HrOperationType';
     const isPosOpen = ref<boolean>(false)
     function closePositions(){
         isPosOpen.value = false
     }
+    
     const employeesList = ref<Employee[]>([])
     const operationsList = ref<fullHrOperation[]>([])
-
     const employeesLastOperations = computed<Employee[]>(() => {
         let emps: Employee[] = employeesList.value
         let ops: fullHrOperation[] = operationsList.value
@@ -25,9 +28,56 @@
         }) 
     })
     
+    
+    function chooseEditedEmployee(emp: Employee, type: HrOperationType){
+        refresh()
+        editOperation.value.employee_id = emp.employee_id
+        editOperation.value.operation_type = type
+        if (emp.last_operation){
+            editOperation.value.department_id = emp.last_operation.department.department_id
+            editedEmployeeOrganizationId.value = emp.last_operation.department.organization.organization_id
+            editOperation.value.position_id = emp.last_operation.position.position_id
+            editOperation.value.salary = emp.last_operation.salary
+        }
+
+    }
+    async function doOperation(){
+        try{
+            await createOperation(editOperation.value)
+            refresh()
+            editOperation.value = {
+                employee_id: 0,
+                department_id: 0,
+                position_id: 0,
+                salary: 0,
+                operation_type: HrOperationType.FIRE
+            }
+        }catch(err){
+            console.error(err)
+        }
+    }
+    async function fire(emp: Employee) {
+        chooseEditedEmployee(emp, HrOperationType.FIRE)
+        await doOperation()
+    }
+
+    const editOperation = ref<Omit<HrOperation,'hr_operation_id'>>({
+        employee_id: 0,
+        department_id: 0,
+        position_id: 0,
+        salary: 0,
+        operation_type: HrOperationType.FIRE
+    })
+    const editedEmployeeOrganizationId = ref<number>(0)
+    const departmentsList = ref<Department[]>([])
+    const positionsList = ref<Position[]>([])
 
     async function refresh(){
         try{
+            positionsList.value = await getAllPositions()
+            if(editedEmployeeOrganizationId.value){
+                departmentsList.value = await getAllForOrgFlat(editedEmployeeOrganizationId.value)
+            }
             employeesList.value = await getAllEmployees()
             operationsList.value = await getAllOperations()
         }catch(err){
@@ -60,16 +110,80 @@
         <div class="table-header">Действия</div>
         
         <template v-for="emp in employeesLastOperations" :key="emp.employee_id">
-            <div class="table-item">{{ emp.last_name }} {{emp.first_name }} {{ emp.middle_name }}</div>
-            <div class="table-item">{{ emp.last_operation?.department.organization.name || '-' }}</div>
-            <div class="table-item">{{ emp.last_operation?.department.name || '-' }}</div>
-            <div class="table-item">{{emp.last_operation?.position.name || '-'}}</div>
-            <div class="table-item">{{ emp.last_operation?.salary || '-' }} ₽</div>
-            <div class="button-row table-item" >
-                <button>Изменить</button>
-                <button>Уволить</button>
+            <div class="table-item" 
+            :class="{'fired': emp.last_operation?.operation_type === HrOperationType.FIRE}">
+                {{ emp.last_name }} {{emp.first_name }} {{ emp.middle_name }}
+            </div>
+            <div class="table-item"
+            :class="{'fired': emp.last_operation?.operation_type === HrOperationType.FIRE}"
+            >
+                {{ emp.last_operation?.department.organization.name || '-' }}
+            </div>
+
+            <div
+                :class="{'fired': emp.last_operation?.operation_type === HrOperationType.FIRE}"
+                v-show="!(editOperation.operation_type == HrOperationType.CHANGE_DEPARTMENT && editOperation.employee_id == emp.employee_id)"
+                class="table-item table-item-edit">
+                {{ emp.last_operation?.department.name || '-' }}
+                <img src="../assets/edit.png" class="icon"  @click="chooseEditedEmployee(emp, HrOperationType.CHANGE_DEPARTMENT)">
+            </div>
+            <form
+                @submit.prevent="doOperation"
+                v-show="editOperation.operation_type == HrOperationType.CHANGE_DEPARTMENT && editOperation.employee_id == emp.employee_id"
+                class="table-item table-item-edit form-inline" >
+                 <select v-model="editOperation.department_id">
+                    <option value="" disabled selected></option>
+                    <option v-for="dep in departmentsList" 
+                    :key="dep.department_id"
+                    :value="dep.department_id">
+                    {{ dep.name }}
+                    </option>
+                </select>
+                <button>OK</button>
+            </form>
+
+            <div 
+            :class="{'fired': emp.last_operation?.operation_type === HrOperationType.FIRE}"
+            v-show="!(editOperation.operation_type == HrOperationType.CHANGE_POSITION && editOperation.employee_id == emp.employee_id)"
+            class="table-item table-item-edit">
+                {{emp.last_operation?.position.name || '-'}}
+                <img src="../assets/edit.png" class="icon" @click="chooseEditedEmployee(emp, HrOperationType.CHANGE_POSITION)">
+            </div>
+            <form 
+                @submit.prevent="doOperation"
+                v-show="editOperation.operation_type == HrOperationType.CHANGE_POSITION && editOperation.employee_id == emp.employee_id"
+                class="table-item table-item-edit form-inline">
+                <select v-model="editOperation.position_id">
+                    <option value="" disabled selected></option>
+                    <option v-for="pos in positionsList" 
+                    :key="pos.position_id"
+                    :value="pos.position_id">
+                    {{ pos.name }}
+                    </option>
+                </select>
+                <button>OK</button>
+            </form>
+
+            <div 
+            :class="{'fired': emp.last_operation?.operation_type === HrOperationType.FIRE}"
+            v-show="!(editOperation.operation_type == HrOperationType.CHANGE_SALARY && editOperation.employee_id == emp.employee_id)"
+            class="table-item table-item-edit">
+                {{ emp.last_operation?.salary || '-' }} ₽
+                <img src="../assets/edit.png" class="icon" @click="chooseEditedEmployee(emp, HrOperationType.CHANGE_SALARY)">
+            </div>
+            <form 
+            @submit.prevent="doOperation"
+            v-show="editOperation.operation_type == HrOperationType.CHANGE_SALARY && editOperation.employee_id == emp.employee_id"
+            class="table-item table-item-edit form-inline">
+                <input 
+                type="numeric"
+                v-model="editOperation.salary">
+                <button>OK</button>
+            </form>
+
+            <div class="button-row table-item" :class="{'fired': emp.last_operation?.operation_type === HrOperationType.FIRE}">
+                <button @click="fire(emp)">Уволить</button>
                 <router-link :to="`/employee/${emp.employee_id}`"><button>Информация</button></router-link>
-                
             </div>
         </template>
         
