0650f78c94ab58a6f12f950b6b581a5f1ba85d15
a.isaeva <isaeva.nastya04@gmail.com>
2026-06-24 10:29:10 +0300

    Добавлены адаптивные стили для шапки

--- app/frontend/src/Header.vue
+++ app/frontend/src/Header.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-import { currentUser } from './currentUser';
-import { UserRoles } from '../../enums/UserRoles';
-
+    import { computed, ref } from 'vue';
+    import { useRoute } from 'vue-router';
+    import { currentUser } from './currentUser';
+    import { UserRoles } from '../../enums/UserRoles';
+    const isOpen = ref<boolean>(false)
     const route = useRoute()
     const pageTitle = computed(()=>{
         return route.meta.title || "HR-система"
@@ -13,14 +13,19 @@ import { UserRoles } from '../../enums/UserRoles';
 
 <template>
     <div class="header">
-        <h1>{{ pageTitle }}</h1>
-        <nav class="nav-panel">
+        <div class="header-line">
+            <h1>{{ pageTitle }}</h1>
+            <button v-if="isOpen" @click="isOpen = false" class="burgerButton">✕</button>
+            <button v-if="!isOpen" @click="isOpen = true" class="burgerButton">☰</button>
+        </div>
+        
+        <nav :class="['nav-panel', {'active-panel':isOpen}]">
             <router-link to="/" class="nav-item">Сотрудники</router-link>
             <router-link to="/organizations" class="nav-item">Организации и отделы</router-link>
             <router-link to="/hr-operations" class="nav-item">Кадровые операции</router-link>
             <router-link to="/history" class="nav-item">История операций</router-link>
-            
             <router-link to="/users" class="nav-item" v-if="currentUser?.role == UserRoles.ADMIN">Пользователи</router-link>
+            
         </nav>
     </div>
 </template>
